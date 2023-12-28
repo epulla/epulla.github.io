9c10e1fe50119e36ea1d9afde48147820b707826
epulla-hm <epulla@happymoney.com>
2023-12-28 14:45:26 -0500

    add: Astro Github pages configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://epulla.github.io',
+  base: "/",
   integrations: [tailwind(), svelte()]
 });
