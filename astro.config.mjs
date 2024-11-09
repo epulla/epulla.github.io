871b6a689088e0dacb5f2d2fc3e51b40f5ce34e8
epulla <erickpulladsi@gmail.com>
2024-11-09 17:23:42 -0500

    refactor: replace `svelte` with `react`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://epulla.github.io',
+  site: "https://epulla.github.io",
   base: "/",
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), react()],
 });
