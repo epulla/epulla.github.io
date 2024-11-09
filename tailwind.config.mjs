fba50176508bf93f0c39ac49e340e69198a18ffa
epulla <erickpulladsi@gmail.com>
2024-11-09 17:34:36 -0500

    feat: add daisyui in tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,28 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-    "./src/layouts/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-  ],
-  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      colors: {
-        razzmatazz: {
-          50: "#fff0f7",
-          100: "#ffe4f2",
-          200: "#ffc8e7",
-          300: "#ff9cd1",
-          400: "#ff5fb0",
-          500: "#ff3090",
-          600: "#ec0964",
-          700: "#d70050",
-          800: "#b10341",
-          900: "#93083a",
-          950: "#5b001e",
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  // daisyUI config
+  daisyui: {
+    themes: [
+      {
+        acid: {
+          ...require("daisyui/src/theming/themes")["acid"],
+          neutral: "#c7cadc",
         },
       },
-    },
+      "dracula",
+    ],
   },
-  plugins: [],
+  darkMode: ["selector", '[data-theme="dracula"]'],
 };
