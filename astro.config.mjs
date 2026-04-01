63d8060c6f2f106ad7753e96a0e10b8ae07337de
epulla <erickpulladsi@gmail.com>
2026-04-01 09:40:59 -0500

    fix: update Astro configuration to use Tailwind CSS with Vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://epulla.github.io",
   base: "/",
-  integrations: [tailwind(), react()],
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
